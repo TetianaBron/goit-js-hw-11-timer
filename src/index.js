f57e6f9ac3dc209563897f798d36d01907efd32b
Tetiana Bronitska <tanya.nest87@gmail.com>
2020-11-08 22:07:49 +0200

    Timer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
 import './styles.css';
+
+const refs = {
+    days: document.querySelector('span[data-value="days"]'),
+    hours: document.querySelector('span[data-value="hours"]'),
+    mins: document.querySelector('span[data-value="mins"]'),
+    secs: document.querySelector('span[data-value="secs"]'),
+}
+
+
+class CountdownTimer {
+    constructor({ onTick }) {
+        this.intervalId = null;
+    }
+    start() {
+        this.intervalId = setInterval(() => {
+            const currentTime = Date.now();
+            const startTime = Date.now();
+            const deltaTime = currentTime - startTime;
+            const time = this.getTimeComponents(deltaTime);
+
+            this.onTick(time);
+        }, 1000);
+    }
+    
+    getTimeComponents(time) {
+        const days = this.pad(Math.floor((time / (1000 * 60 * 60 * 24))));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    
+    return { hours, mins, secs };
+    }
+
+     /*
+   * Принимает число, приводит к строке и добавляет в начало 0 если число меньше 2-х знаков
+   */
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+const timer = new CountdownTimer({
+  onTick: updateClockface,
+ /// selector: '#timer-1',
+  //targetDate: new Date('Jun 16, 2021'),
+});
+
+function updateClockface({ days, hours, mins, secs }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+}
+
